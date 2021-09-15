fea8f9c8cadab77cda9d324937612a644926cf26
Ampderg <guywithamp@hotmail.com>
2021-09-15 10:43:58 -0700

    Login working

--- Assets/Scripts/ServerLink.cs
+++ Assets/Scripts/ServerLink.cs
@@ -158,7 +158,6 @@ public class ServerLink : MonoBehaviour
         {
             client.Connect(remoteEP);
             Debug.Log("Connected!");
-            SslStream stream = new SslStream(new NetworkStream(client), false, new RemoteCertificateValidationCallback(ValidateServerCertificate), null);
             this.socket = client;
             return true;
         }
@@ -175,20 +174,6 @@ public class ServerLink : MonoBehaviour
         }
     }
 
-    public static bool ValidateServerCertificate(
-              object sender,
-              X509Certificate certificate,
-              X509Chain chain,
-              SslPolicyErrors sslPolicyErrors)
-    {
-        if (sslPolicyErrors == SslPolicyErrors.None)
-            return true;
-
-        Debug.LogError(string.Format("Certificate error: {0}", sslPolicyErrors));
-
-        // Do not allow this client to communicate with unauthenticated servers.
-        return false;
-    }
 
     private void ValidateUserInfo()
     {
